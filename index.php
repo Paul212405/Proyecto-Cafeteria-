0021c66dd3599a19973e0f4cd3f66829e8a98603
Paul <mallquiriverap@gmail.com>
2023-02-12 23:16:41 -0500

    estructura completa

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-include_once("components/head.php");
+include_once("utils/head.php");
 ?>
-<link href="css/estilos.css" rel="stylesheet">
 <h1 class="mr-auto font-bold mt-10 bg-black">Hola mundo</h1>
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/**@type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./*.{html,js,php}','./utils/**/*.{html,js,php}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
