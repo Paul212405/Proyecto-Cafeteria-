2f757c2f41cea10c38b72ecc0fb736e8dae8ec5c
camila2117 <camilamoralessusanibar@gmail.com>
2023-03-28 02:37:36 -0500

    cami

--- Admin/paginas/resumen.php
+++ Admin/paginas/resumen.php
@@ -2,47 +2,71 @@
 <div class="block">
     <div class="lg:ml-72 p-6 ">
         <div class="flex justify-center items-center w-full">
-            <div class="grid grid-cols-3 gap-6">
+            <div class="grid lg:grid-cols-3 grid-cols-2  gap-6">
                 <div class="col-span-1 w-full">
-                    <h1 class="font-dmserif text-2xl font-bold text-red-300 w-full text-center p-2">USUARIOS</h1>
-                    <img class="w-65 h-150" src="../Public/images/negocio/usuario1.png" />
-                    <div class="flex justify-center items-center w-full text-center p-2">
-                        <button onclick="cargarcomponente('./paginas/usuario.php','contenedor_admin')" class="text-center inline-block w-full px-4 py-2 rounded hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-1 focus:ring-gray-300 focus:text-white ">MÁS INFO +</button>
+                    <div class="flex justify-center items-center flex-wrap w-full lg:px-8 px-5 gap-4">
+                        <h1 class="font-dmserif text-2xl font-bold text-red-300 w-full text-center p-2">USUARIOS</h1>
+                        <div class="w-full px-4">
+                            <img class="w-full h-40" src="../Public/images/negocio/usuario1.png" />
+                        </div>
+                        <div class="flex justify-center items-center w-full text-center p-2">
+                            <button onclick="cargarcomponente('./paginas/usuario.php','contenedor_admin')" class="text-center inline-block w-full px-4 py-2 rounded hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-1 focus:ring-gray-300 focus:text-white ">MÁS INFO +</button>
+                        </div>
                     </div>
                 </div>
                 <div class="col-span-1 w-full">
-                    <h1 class="font-dmserif text-2xl font-bold text-red-300 w-full text-center p-1">CLIENTES</h1>
-                    <img class="w-65 h-100" src="../Public/images/negocio/cliente1.png" />
-                    <div class="flex justify-center items-center w-full text-center p-1">
-                        <button onclick="cargarcomponente('./paginas/cliente.php','contenedor_admin')" class="text-center inline-block w-full px-4 py-2 rounded hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-1 focus:ring-gray-300 focus:text-white ">MÁS INFO +</button>
+                    <div class="flex justify-center items-center flex-wrap w-full lg:px-8 px-5 gap-4">
+                        <h1 class="font-dmserif text-2xl font-bold text-red-300 w-full text-center p-2">CLIENTES</h1>
+                        <div class="w-full px-4">
+                            <img class="w-full h-40" src="../Public/images/negocio/cliente1.png" />
+                        </div>
+                        <div class="flex justify-center items-center w-full text-center p-2">
+                            <button onclick="cargarcomponente('./paginas/cliente.php','contenedor_admin')" class="text-center inline-block w-full px-4 py-2 rounded hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-1 focus:ring-gray-300 focus:text-white ">MÁS INFO +</button>
+                        </div>
                     </div>
                 </div>
                 <div class="col-span-1 w-full">
-                    <h1 class="font-dmserif text-2xl font-bold text-red-300 w-full text-center p-2">TESTIMONIOS</h1>
-                    <img class="w-40 h-100" src="../Public/images/negocio/testimonio2.png" />
-                    <div class="flex justify-center items-center w-full text-center p-2 ">
-                        <button onclick="cargarcomponente('./paginas/testimonio.php','contenedor_admin')" class="text-center inline-block w-full px-4 py-2 rounded hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-1 focus:ring-gray-300 focus:text-white ">MÁS INFO +</button>
+                    <div class="flex justify-center items-center flex-wrap w-full lg:px-14 px-10 gap-4">
+                        <h1 class=" font-dmserif text-2xl font-bold text-red-300 w-full text-center p-2">TESTIMONIOS</h1>
+                        <div class="w-full px-4">
+                            <img class="w-full h-40" src="../Public/images/negocio/testimonio1.png" />
+                        </div>
+                        <div class="flex justify-center items-center w-full text-center p-2 ">
+                            <button onclick="cargarcomponente('./paginas/testimonio.php','contenedor_admin')" class="text-center inline-block w-full px-4 py-2 rounded hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-1 focus:ring-gray-300 focus:text-white ">MÁS INFO +</button>
+                        </div>
                     </div>
                 </div>
                 <div class="col-span-1 w-full">
-                    <h1 class="font-dmserif text-2xl font-bold text-red-300 w-full text-center p-1">PRODUCTOS</h1>
-                    <img class="w-40 h-100" src="../Public/images/negocio/productos1.png" />
-                    <div class="flex justify-center items-center w-full text-center p-2">
-                        <button onclick="cargarcomponente('./paginas/producto.php','contenedor_admin')" class="text-center inline-block w-full px-4 py-2 rounded hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-1 focus:ring-gray-300 focus:text-white ">MÁS INFO +</button>
+                    <div class="flex justify-center items-center flex-wrap w-full lg:px-8 px-5 gap-4">
+                        <h1 class=" font-dmserif text-2xl font-bold text-red-300 w-full text-center p-2">PRODUCTOS</h1>
+                        <div class="w-full px-4">
+                            <img class="w-full h-40" src="../Public/images/negocio/productos1.png" />
+                        </div>
+                        <div class="flex justify-center items-center w-full text-center p-2">
+                            <button onclick="cargarcomponente('./paginas/producto.php','contenedor_admin')" class="text-center inline-block w-full px-4 py-2 rounded hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-1 focus:ring-gray-300 focus:text-white ">MÁS INFO +</button>
+                        </div>
                     </div>
                 </div>
                 <div class="col-span-1 w-full">
-                    <h1 class="font-dmserif text-2xl font-bold text-red-300 w-full text-center p-2">CATEGORIAS</h1>
-                    <img class="w-60 h-100" src="../Public/images/negocio/categoria2.png" />
-                    <div class="flex justify-center items-center w-full text-center p-2">
-                        <button onclick="cargarcomponente('./paginas/categoria.php','contenedor_admin')" class="text-center inline-block w-full px-4 py-2 rounded hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-1 focus:ring-gray-300 focus:text-white ">MÁS INFO +</button>
+                    <div class="flex justify-center items-center flex-wrap w-full lg:px-8 px-5 gap-4">
+                        <h1 class="font-dmserif text-2xl font-bold text-red-300 w-full text-center p-2">CATEGORIAS</h1>
+                        <div class="w-full px-4">
+                            <img class="w-full h-40" src="../Public/images/negocio/categoria2.png" />
+                        </div>
+                        <div class="flex justify-center items-center w-full text-center p-2">
+                            <button onclick="cargarcomponente('./paginas/categoria.php','contenedor_admin')" class="text-center inline-block w-full px-4 py-2 rounded hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-1 focus:ring-gray-300 focus:text-white ">MÁS INFO +</button>
+                        </div>
                     </div>
                 </div>
                 <div class="col-span-1 w-full">
-                    <h1 class="font-dmserif text-2xl font-bold text-red-300 w-full text-center p-1">PEDIDOS</h1>
-                    <img class="w-40 h-100" src="../Public/images/negocio/pedido1.png" />
-                    <div class="flex justify-center items-center w-full text-center p-2">
-                        <button onclick="cargarcomponente('./paginas/pedido.php','contenedor_admin')" class="text-center inline-block w-full px-4 py-2 rounded hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-1 focus:ring-gray-300 focus:text-white ">MÁS INFO +</button>
+                    <div class="flex justify-center items-center flex-wrap w-full lg:px-8 px-5 gap-4">
+                        <h1 class="font-dmserif text-2xl font-bold text-red-300 w-full text-center p-2">PEDIDOS</h1>
+                        <div class="w-full px-4">
+                            <img class="w-full h-40" src="../Public/images/negocio/pedido1.png" />
+                        </div>
+                        <div class="flex justify-center items-center w-full text-center p-2">
+                            <button onclick="cargarcomponente('./paginas/pedido.php','contenedor_admin')" class="text-center inline-block w-full px-4 py-2 rounded hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-1 focus:ring-gray-300 focus:text-white ">MÁS INFO +</button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -50,8 +74,3 @@
     </div>
 
 </div>
-<!--div class="relative z-0 float-left -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none bg-cover" data-te-carousel-item data-te-carousel-active>
-            <!--Para el mensaje>
-            <h1 class="z-[1] absolute top-[70%] left-[38%] sm:top-1/3 sm:left-[20%] text-white font-bold text-6xl text-center sm:text-7xl sm:text-right">La Casa del Café</h1>
-            <div class="bg-[url('./../Public/images/negocio/slider_01.jpg')] block w-full h-[125vh] bg-cover opacity-80" alt="slider_01"></div>
-        </div-->
