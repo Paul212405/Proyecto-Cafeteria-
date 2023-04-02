8b2e6756dc5a2197034dee206f5d1c4152cf025b
camila2117 <mallquiriverap@gmail.com>
2023-04-02 12:31:56 -0500

    Correccion de margenes

--- Admin/paginas/testimonio_add.php
+++ Admin/paginas/testimonio_add.php
@@ -8,12 +8,14 @@ $sql = "SELECT * FROM tb_producto";
 $result_producto = $con->query($sql);
 ?>
 <section class="block">
-    <div class="justify-center lg:ml-40 -ml-28  lg:px-5 px-5">
+    <div class="justify-center lg:ml-72 lg:px-5 px-5">
         <div class="flex justify-center items-center mb-4">
-            <div class="bg-white rounded-lg shadow-xl grow sm:grow-0 sm:w-[50%]">
-                <div class="bg-black w-full rounded-t-lg py-6 shadow-2xl">
+            <div class="bg-white rounded-lg shadow-xl grow sm:grow-0 sm:w-[65%] lg:w-[60%]">
+                <div class="bg-black w-full rounded-t-lg py-6 shadow-2xl px-5">
                     <h1 class="text-center font-semibold text-lg lg:text-xl pb-2 text-white/90">DATOS DEL TESTIMONIO ::: AGREGAR DATOS</h1>
-                    <img class="flex w-full lg:px-56 px-24" src="../Public/images/negocio/testimonio1.png" />
+                    <div class="flex justify-center items-center w-full">
+                        <img class="w-28 h-28" src="../Public/images/negocio/testimonio1.png" />
+                    </div>
                 </div>
                 <form class="px-4 py-5" method="POST" action="./../back_end/controladores/testimonio/p_add_testimonio.php">
                     <div class="w-full block">
@@ -60,15 +62,15 @@ $result_producto = $con->query($sql);
                             </select>
                         </div>
                     </div>
-                    <div class="flex justify-center lg:px-0 ">
-                        <div class="flex justify-center px-1">
-                            <button type="submit" name="edit" class="w-full px-6 rounded-lg bg-green-800 py-3  text-sm lg:text-lg font-bold text-white"><i class="fa fa-save mr-2"></i> Guardar Datos</button>
+                    <div class="flex justify-center lg:px-0">
+                        <div class="flex justify-center px-1 flex-1">
+                            <button type="submit" name="edit" class="w-full px-6 rounded-lg bg-green-800 py-3  text-sm font-bold text-white"><i class="fa fa-save mr-2"></i> Guardar Datos</button>
                         </div>
-                        <div class="flex justify-center ">
-                            <button onclick="cargarcomponente('./paginas/close.php','contenedor_testimonio');event.preventDefault();" class="w-full px-6 rounded-lg bg-red-700 py-3 text-sm lg:text-lg font-bold text-white"><i class="fa fa-power-off mr-2"></i> Cancelar</button>
+                        <div class="flex justify-center flex-1">
+                            <button onclick="cargarcomponente('./paginas/close.php','contenedor_testimonio');event.preventDefault();" class="w-full px-6 rounded-lg bg-red-700 py-3 text-sm font-bold text-white"><i class="fa fa-power-off mr-2"></i> Cancelar</button>
                         </div>
                     </div>
-                    
+
                 </form>
             </div>
         </div>
--- Admin/paginas/usuario_add.php
+++ Admin/paginas/usuario_add.php
@@ -1,31 +1,33 @@
 <section class="block">
-    <div class=" justify-center lg:ml-40 -ml-44  lg:px-5 px-5">
+    <div class="justify-center lg:ml-72 lg:px-5 px-5">
         <div class="flex justify-center items-center mb-4">
-            <div class="bg-white rounded-lg shadow-xl grow sm:grow-0 sm:w-[40%]">
+            <div class="bg-white rounded-lg shadow-xl grow sm:grow-0 sm:w-[70%] lg:w-[60%]">
                 <div class="bg-black w-full rounded-t-lg py-6 shadow-2xl">
                     <h1 class="text-center font-semibold text-lg lg:text-xl pb-2 text-white/90">DATOS DEL USUARIO ::: AGREGAR DATOS</h1>
-                    <img class="flex w-full lg:px-36 px-10" src="../Public/images/negocio/usuario1.png" />
+                    <div class="flex justify-center items-center w-full">
+                        <img class="w-32 h-28" src="../Public/images/negocio/usuario1.png" />
+                    </div>
                 </div>
-                <form class=" lg:px-20 px-2 py-5 " method="POST" action="./../back_end/controladores/usuario/p_add_usuario.php">
-                    <div class="w-full lg:w-96 block">
+                <form class=" lg:px-6 px-4 py-5 " method="POST" action="./../back_end/controladores/usuario/p_add_usuario.php">
+                    <div class="w-full block">
                         <label class="text-lg font-semibold pb-4">Nombre</label>
                         <div class="w-full my-4">
                             <input type="text" class="w-full h-8 p-5 font-light border-2 border-black/10 rounded-lg focus:border-black/80 focus:outline-none transition-all duration-500" name="txt_nombre" placeholder="Nombre de usuario" required>
                         </div>
                     </div>
-                    <div class="w-full lg:w-96 block">
+                    <div class="w-full block">
                         <label class="text-lg font-semibold pb-4">Contraseña</label>
                         <div class="w-full my-4">
                             <input type="password" class="w-full h-8 p-5 font-light border-2 border-black/10 rounded-lg focus:border-black/80 focus:outline-none transition-all duration-500" name="txt_pass" placeholder="Ingrese su contraseña" required>
                         </div>
                     </div>
-                    <div class="w-full lg:w-96 block">
+                    <div class="w-full block">
                         <label class="text-lg font-semibold pb-4">Email</label>
                         <div class="w-full my-4">
                             <input type="email" class="w-full h-8 p-5 font-light border-2 border-black/10 rounded-lg focus:border-black/80 focus:outline-none transition-all duration-500" name="txt_email" placeholder="example@gmail.com" required>
                         </div>
                     </div>
-                    <div class="w-full lg:w-96 ">
+                    <div class="w-full">
                         <label class="text-lg font-semibold pb-4 ">Rol</label>
                         <div class="w-full my-4">
                             <select name="tipo" class="w-full sm:w-1/2 h-12 px-5 py-3 font-light border-2 text-black border-black/10 rounded-lg focus:border-black/80 focus:outline-none transition-all duration-500 cursor-pointer">
@@ -39,12 +41,12 @@
                         </div>
                     </div>
 
-                    <div class="flex justify-center lg:px-0 ">
-                        <div class="flex justify-center px-1">
-                            <button type="submit" name="edit" class="w-full px-6 rounded-lg bg-green-800 py-3  text-sm lg:text-lg font-bold text-white"><i class="fa fa-save mr-2"></i> Guardar Datos</button>
+                    <div class="flex justify-between lg:px-0 ">
+                        <div class="flex justify-center px-1 flex-1">
+                            <button type="submit" name="edit" class="w-full px-4 rounded-lg bg-green-800 py-3  text-sm lg:text-sm font-bold text-white"><i class="fa fa-save mr-2"></i> Guardar Datos</button>
                         </div>
-                        <div class="flex justify-center ">
-                            <button onclick="cargarcomponente('./paginas/close.php','contenedor_usuario');event.preventDefault();" class="w-full px-6 rounded-lg bg-red-700 py-3 text-sm lg:text-lg font-bold text-white"><i class="fa fa-power-off mr-2"></i> Cancelar</button>
+                        <div class="flex justify-center flex-1">
+                            <button onclick="cargarcomponente('./paginas/close.php','contenedor_usuario');event.preventDefault();" class="w-full px-4 rounded-lg bg-red-700 py-3 text-sm lg:text-sm font-bold text-white"><i class="fa fa-power-off mr-2"></i> Cancelar</button>
                         </div>
                     </div>
                 </form>
